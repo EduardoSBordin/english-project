fc2e4cbff13b318520b33756f622b5caa9d9e484
Eduardo Bordin <99068107+EduardoSBordin@users.noreply.github.com>
2023-02-10 23:39:14 -0300

    v5.1

--- script.js
+++ script.js
@@ -1,106 +1,170 @@
-const a1 = document.querySelector('#a1');
-const a2 = document.querySelector('#a2');
-const a3 = document.querySelector('#a3');
+const checkbox1 = document.querySelector('#a_1');
+const checkbox2 = document.querySelector('#a_2');
+const checkbox3 = document.querySelector('#a_3');
 
 const btnResult = document.querySelector('#btnResult');
 const btnRemoveScore = document.querySelector('#btnRemoveScore');
 
 let result = document.querySelector('#result');
-
 const scoreTxt = document.querySelector('#score');
 
-let time = 10;
 let score = 0;
+let i = 0;
+let time = 10;
 
-function displayTryAgain(){
-
-
-        const tag = document.createElement('h1');
-            tag.innerHTML = 'Try Again';
-            tag.style.color = 'grey';
-            tag.style.transition = "0.3s";
-            
-            result.append(tag);
-
-       const getInterval = setInterval(() => {
-            time--;
-            if(time <= 0){
-
-                console.log('OK');
-                clearInterval(getInterval);
-                time = 10;
-                tag.remove();
-                btnResult.disabled = false;
-                // btnResult.style.background = 'white';
-                // btnResult.style.color = 'black';
-
-            }
-            console.log(time);
-        }, 100);
-}
 
 function displayRight(){
 
+    const tag = document.createElement('h1');
+    tag.innerHTML = 'Right';
+    tag.style.color = 'grey';
+
+    result.append(tag);
+
+    const getInterval = setInterval(() => {
+        time--;
+        if(time <= 0){
+
+            console.log('OK');
+            clearInterval(getInterval);
+            time = 10;
+            tag.remove();
+            btnResult.disabled = false;
+
+        }
+        console.log(time);
+    }, 100);
+}
+
+function displayTryAgain(){
 
     const tag = document.createElement('h1');
-            tag.innerHTML = "That's right";
-            
-            result.append(tag);
+    tag.innerHTML = 'Try Again';
+    tag.style.color = 'grey';
 
-        
-       const getInterval = setInterval(() => {
-            time--;
-            if(time <= 0){
+    result.append(tag);
 
-                console.log('OK');
-                clearInterval(getInterval);
-                time = 10;
-                tag.remove();
-                btnResult.disabled = false;
-                // btnResult.style.background = 'white';
-                // btnResult.style.color = 'black';
-            }
-            console.log(time);
-        }, 100);
+    const getInterval = setInterval(() => {
+        time--;
+        if(time <= 0){
+
+            console.log('OK');
+            clearInterval(getInterval);
+            time = 10;
+            tag.remove();
+            btnResult.disabled = false;
+
+        }
+        console.log(time);
+    }, 100);
 }
+
 function localS(){
 
     localStorage.setItem('getScore', score);
     scoreTxt.innerHTML = localStorage.getItem('getScore');
 }
 
-btnResult.addEventListener('click', () => {
+checkbox1.addEventListener('click', () => {
 
-    btnResult.disabled = true;
-    
-    if(a1.checked){
+    i++;
+    if(i <= 1){
+        checkbox2.disabled = true;
+        checkbox2.style.boxShadow = 'rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset';
+        checkbox2.style.border = 'none';
+
+        checkbox3.disabled = true;
+        checkbox3.style.boxShadow = 'rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset';
+        checkbox3.style.border = 'none';
+    }else{
+        i = 0;
+        checkbox2.disabled = false;
+        checkbox2.style.boxShadow = '-10px -10px 15px rgba(255, 255, 255, 0.5), 10px 10px 15px rgba(70, 70, 70, 0.12)';
+        checkbox2.style.border = '8px solid #ececec';
+
+        checkbox3.disabled = false;
+        checkbox3.style.boxShadow = '-10px -10px 15px rgba(255, 255, 255, 0.5), 10px 10px 15px rgba(70, 70, 70, 0.12)';
+        checkbox3.style.border = '8px solid #ececec';
+    }
+    console.log(i);
+})
+
+checkbox2.addEventListener('click', () => {
+
+    i++;
+    if(i <= 1){
+        checkbox1.disabled = true;
+        checkbox1.style.boxShadow = 'rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset';
+        checkbox1.style.border = 'none';
+
+        checkbox3.disabled = true;
+        checkbox3.style.boxShadow = 'rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset';
+        checkbox3.style.border = 'none';
         
-        //a2.disabled = true;
-        score--;
-        displayTryAgain();
-        localS();
+    }else{
+        i = 0;
+        checkbox1.disabled = false;
+        checkbox1.style.boxShadow = '-10px -10px 15px rgba(255, 255, 255, 0.5),10px 10px 15px rgba(70, 70, 70, 0.12)';
+        checkbox1.style.border = '8px solid #ececec';
 
-    } else if(a2.checked){
-
-        score--;
-        displayTryAgain();
-        localS();
+        checkbox3.disabled = false;
+        checkbox3.style.boxShadow = '-10px -10px 15px rgba(255, 255, 255, 0.5),10px 10px 15px rgba(70, 70, 70, 0.12)';
+        checkbox3.style.border = '8px solid #ececec';
     }
-     else if(a3.checked){
-
-         score++;
-        displayRight();
-        localS();
-
-    }
-
-    if(score <= 0){
-        score = 0;
-        scoreTxt.innerHTML = score;
-    }
-  
+    console.log(i);
 });
 
+checkbox3.addEventListener('click', () => {
+
+    i++;
+    if(i <= 1){
+        checkbox1.disabled = true;
+        checkbox1.style.boxShadow = 'rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset';
+        checkbox1.style.border = 'none';
+
+        checkbox2.disabled = true;
+        checkbox2.style.boxShadow = 'rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset';
+        checkbox2.style.border = 'none';
+    }else{
+        i = 0;
+        checkbox1.disabled = false;
+        checkbox1.style.boxShadow = '-10px -10px 15px rgba(255, 255, 255, 0.5),10px 10px 15px rgba(70, 70, 70, 0.12)';
+        checkbox1.style.border = '8px solid #ececec';
+
+        checkbox2.disabled = false;
+        checkbox2.style.boxShadow = '-10px -10px 15px rgba(255, 255, 255, 0.5),10px 10px 15px rgba(70, 70, 70, 0.12)';
+        checkbox2.style.border = '8px solid #ececec';
+    }
+    console.log(i);
+});
+
+btnResult.addEventListener('click', () => {
+    btnResult.disabled = true;
+    
+        if(checkbox1.checked && checkbox2.checked){
+            
+            alert('Choose just 1 option');
+        }
+        else if(checkbox1.checked){
+            score--;
+            displayTryAgain();
+            localS();
+        }else if(checkbox2.checked){
+            score--;
+            displayTryAgain();
+            localS();
+        }else if(checkbox3.checked){
+            score++;
+            displayRight();
+            localS();
+        }
+    
+        if(score <= 0){
+            score = 0;
+            scoreTxt.innerHTML = score;
+        }
+    
+    });
 scoreTxt.innerHTML = localStorage.getItem('getScore');
 
 btnRemoveScore.addEventListener('click', () => {
